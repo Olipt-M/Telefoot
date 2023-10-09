3af716d645bb94cf7d4260babf939dbe36dc6f4c
Olivier Mignot <olipt.mignot@gmail.com>
2023-10-09 13:22:03 +0200

    fixed bug with check session hijacking

--- src/view/user.php
+++ src/view/user.php
@@ -12,6 +12,16 @@ class UserView
 
   public function render()
   {
+    session_start();
+    // Check if user is connected and avoid session hijacking
+    if (!isset($_SESSION["user"]) || ($_SESSION["user"]["ip"] != $_SERVER["REMOTE_ADDR"])) {
+      header('Location: home');
+    }
+
+    // echo ($_SESSION["user"]["ip"] != $_SERVER["REMOTE_ADDR"]);
+    // echo $_SESSION["user"]["ip"];
+    // echo $_SERVER["REMOTE_ADDR"];
+
     require($this->template);
   }
 }
--- templates/user.php
+++ templates/user.php
@@ -1,15 +1,4 @@
 <?php
-  // Check if user is connected and avoid session hijacking
-  // if (!isset($_SESSION["user"]) || ($_SESSION["user"]["ip"] != $_SERVER["REMOTE_ADDR"])) {
-  //     header('Location: home');
-  // }
-  // echo ($_SESSION["user"]["ip"] != $_SERVER["REMOTE_ADDR"]);
-  // echo $_SESSION["user"]["ip"];
-  // echo $_SERVER["REMOTE_ADDR"];
-
-
-  // PROBLEME $_SESSION["user"]["ip"] EST VIDE, A CAUSE DE LA NAVIGATION PRIVEE ? IMPACT TEST SESSION HIJACKING // Esct-q ue j'ai fait trop de session_start ?
-
   include("header.php");
 ?>
 
